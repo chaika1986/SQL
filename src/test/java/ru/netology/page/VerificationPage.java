0a26a4e5b1882d588f15f22e27493b8ebe668d88
Chaika <chaika1986@mail.ru>
2022-11-22 22:40:10 +0700

    first commit

---
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -0,0 +1,15 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+
+    private SelenideElement verificationCodeField = $("[data-test-id=\"code\"] .input__control");
+    private SelenideElement buttonEnter = $("[data-test-id=\"action-verify\"] .button__text");
+
+    public void input(String code) {
+        verificationCodeField.val(code);
+        buttonEnter.click();
+    }
+}
